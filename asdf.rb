9ba3d1cf4ebaa2d5abab54d8b99eda61da794755
Jack Schultz <jackschultz23@gmail.com>
2014-12-07 15:11:02 -0600

    removing files

--- asdf.rb
+++
@@ -1,3 +0,0 @@
-games = Game.on_date(Date.today)
-pcs = PlayerCost.from_games(games.map(&:id)).positive.primary.healthy
-pcs.each {|pc| puts "#{pc.id},#{pc.player.team_id}, #{pc.position}, #{pc.salary}, #{pc.expected_points}" }
