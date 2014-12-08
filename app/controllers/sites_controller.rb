3ec3ecfdcda4087eb3d25258f202f1917ff0678d
Jack Schultz <jackschultz23@gmail.com>
2014-12-07 20:12:05 -0600

    links to days for each site

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -6,6 +6,8 @@ class SitesController < ApplicationController
 
   def show
     @site = Site.find(params[:id])
+    beginning_of_season = Date.new(2014, 10, 27)
+    @dates = Date.today.downto(beginning_of_season)
   end
 
   private
